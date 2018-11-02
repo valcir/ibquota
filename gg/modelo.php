8a6d77c88344393b430dbdef6458e0c7c5e848c1
Valcir Cabral <valcir.cabral@gmail.com>
2018-11-02 08:45:00 -0300

    update modelo - GG

--- gg/modelo.php
+++ gg/modelo.php
@@ -6,13 +6,33 @@
  * 30/10/2018 - Valcir C.
  *
  * Modelo/template para seguir
- */  
+ */ 
+ 
 include_once 'includes/db.php';
 include_once 'includes/functions.php';
+
+ 
+sec_session_start();
+
+if (login_check($mysqli) == false) {
+  header("Location: ../login.php");
+  exit();
+}
+
+//if ($_SESSION['permissao'] != 0){
+//  header("Location: ../login.php");
+//  exit();
+//}
  
 include 'includes/header.php';
 
 ?>
+
+
+
+
+
+
 
 
 
