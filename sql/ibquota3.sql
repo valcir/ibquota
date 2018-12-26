7851040f12c56bc77630fa28a3b8c5e7e5cc437d
elrafa-42 <silveiragomesr@gmail.com>
2018-12-26 09:12:16 -0200

    fix: Remove criação duplicada de PK da tabela adm_users

--- sql/ibquota3.sql
+++ sql/ibquota3.sql
@@ -244,9 +244,6 @@ CREATE TABLE IF NOT EXISTS `adm_users` (
 
 INSERT INTO `adm_users` (`cod_adm_users`, `nome`, `login`, `email`, `senha`,`permissao`) VALUES
 (1, 'Admin IBQUOTA', 'admin', '', '', 2);
-ALTER TABLE `adm_users` ADD PRIMARY KEY (`cod_adm_users`);
-
-
 
 --
 -- Indexes for dumped tables
